7b4fd287c651530f96ce5a5e9cf4306893f07603
vladmurnik <121230377+vladmurnik@users.noreply.github.com>
2026-03-26 10:16:41 +0300

    Update settings.py

--- World_books/settings.py
+++ World_books/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)5(g13e_@jggiu!1%$h(@65r=7o+&co%r--w3oeqlr*_xpv=7i'
+SECRET_KEY = 'django-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
